baea6841d6dd2bb48e886cab14147c8cea1d3270
epricewillis <111535716+epricewillis@users.noreply.github.com>
2022-10-18 23:51:32 -0500

    escape characters from w3schools

---
+++ escape characters.py
@@ -0,0 +1,16 @@
+txt = 'It\'s alright.'
+print(txt)
+txt = "This will insert one \\ (backslash)."
+print(txt)
+txt = "Hello\nWorld!"
+print(txt)
+txt = "Hello\rWorld!"
+print(txt)
+txt = "Hello\tWorld!"
+print(txt)
+txt = "Hello \bWorld!"
+print(txt)
+txt = "\110\145\154\154\157"
+print(txt)
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt)

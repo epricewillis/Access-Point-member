e6f84b32c3086a314e0284ee13db4a5bcc20d2af
epricewillis <111535716+epricewillis@users.noreply.github.com>
2022-10-29 14:46:36 -0500

    String encode example two from w3schools

---
+++ examples uses ascii encoding, and a character that cannot be encoded, showing the result with different errors.py
@@ -0,0 +1,6 @@
+txt = "My name is Ståle"
+print(txt.encode(encoding="ascii",errors="backskashreplace"))
+print(txt.encode(encoding="ascii",errors="ignore"))
+print(txt.encode(encoding="ascii",errors="namereplace"))
+print(txt.encode(encoding="ascii",errors="replace"))
+print(txt.encode(encoding="ascii",errors="xmlcharrefreplace"))
